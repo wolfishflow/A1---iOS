ba05b0e47696e4b89f1ed59a93411e699058d55a
wolfishflow <xcode@FASTs-MacBook-Pro-7.local>
2016-02-09 15:48:37 -0500

    Back Buttons Work
    Fulfilled function to allow the other views to rewind to home

--- A1AlanSimon/A1AlanSimon/ViewController.m
+++ A1AlanSimon/A1AlanSimon/ViewController.m
@@ -14,6 +14,12 @@
 
 @implementation ViewController
 
+
+-(IBAction)unwindToThisView:(UIStoryboardSegue *)sender
+{
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
